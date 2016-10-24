25b51463d3cef16beb6be5b8a3fc655e9339592b
Jayin Ton <tonjayin@gmail.com>
2016-10-24 18:40:15 +0800

    Cron: 移除使用说明

--- app/Application/Cron/View/Cron/index.php
+++ app/Application/Cron/View/Cron/index.php
@@ -2,15 +2,7 @@
 <Admintemplate file="Common/Head"/>
 <body class="J_scroll_fixed">
 <div class="wrap">
-  <div class="h_a">功能说明</div>
-  <div class="prompt_text">
-    <ol>
-      <li>计划任务是一项使系统在规定时间自动执行某些特定任务的功能。</li>
-      <li>合理设置执行时间，能有效地为服务器减轻负担。</li>
-      <li>触发任务除系统指定的时间外，用户行为也可触发。触发任务的任务周期只是初始值。</li>
-      <li>想要计划任务顺利执行，需要一个触发媒介！ <br />独立主机用户可以在系统增加计划任务间隔20秒执行访问[http://网站地址/index.php?g=Cron&m=Index&a=index]。<br />虚拟主机用户，需要在网站模板中最底部增加一个js调用[&lt;script type="text/javascript" src="http://网站地址/index.php?g=Cron&m=Index&a=index"&gt;&lt;/script&gt;]以游客访问页面的形式触发！</li>
-    </ol>
-  </div>
+  
   <Admintemplate file="Common/Nav"/>
   <div class="table_list">
     <table width="100%">
