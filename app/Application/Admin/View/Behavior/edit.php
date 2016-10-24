6bc31c3e960bf146b88abef51963e6faaffc1f5a
Jayin Ton <tonjayin@gmail.com>
2016-10-24 21:24:08 +0800

    Behavior: Remove document text

--- app/Application/Admin/View/Behavior/edit.php
+++ app/Application/Admin/View/Behavior/edit.php
@@ -3,41 +3,6 @@
 <body class="J_scroll_fixed">
 <div class="wrap">
   <Admintemplate file="Common/Nav"/>
-  <div class="h_a">行为规则使用说明</div>
-  <div class="prompt_text">
-    <p><b>规则定义格式1：</b> </p>
-    <ul style="color:#00F">
-      <li>格式： table:$table|field:$field|condition:$condition|rule:$rule[|cycle:$cycle|max:$max]</li>
-    </ul>
-    <p><b>规则字段解释：</b></p>
-    <ul>
-      <li>table->要操作的数据表，不需要加表前缀</li>
-      <li>field->要操作的字段</li>
-      <li>condition-><literal>操作的条件，目前支持字符串。条件中引用行为参数，使用{$parameter}的形式（该形式只对行为标签参数是为数组的有效，纯碎的参数使用{$self}）！</literal></li>
-      <li>rule->对字段进行的具体操作，目前支持加、减 </li>
-      <li>cycle->执行周期，单位（小时），表示$cycle小时内最多执行$max次 </li>
-      <li>max->单个周期内的最大执行次数（$cycle和$max必须同时定义，否则无效）</li>
-    </ul>
-    <p><b>规则定义格式2：</b> </p>
-    <ul style="color:#00F">
-      <li>格式： phpfile:$phpfile[|module:$module]</li>
-    </ul>
-    <p><b>规则字段解释：</b></p>
-    <ul>
-      <li>phpfile->直接调用已经定义好的行为文件。</li>
-      <li>module->行为所属模块，没有该参数时，自动定位到 app\Common\Behavior 目录。</li>
-    </ul>
-    <p><b>规则定义格式3：</b> </p>
-    <ul style="color:#00F">
-      <li>格式： sql:$sql[|cycle:$cycle|max:$max]</li>
-    </ul>
-    <p><b>规则字段解释：</b></p>
-    <ul>
-      <li>sql-><literal>需要执行的SQL语句，表前缀可以使用“ztbcms_”代替。参数可以使用 {$parameter}的形式（该形式只对行为标签参数是为数组的有效，纯碎的参数使用{$self}）！</literal></li>
-      <li>cycle->执行周期，单位（小时），表示$cycle小时内最多执行$max次 </li>
-      <li>max->单个周期内的最大执行次数（$cycle和$max必须同时定义，否则无效）</li>
-    </ul>
-  </div>
   <form class="J_ajaxForm" action="{:U('Behavior/edit')}" method="post">
     <div class="h_a">基本属性</div>
     <div class="table_full">
