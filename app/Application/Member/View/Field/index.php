afbe1179225b92b1c1184f4d3d27aead8da01b61
Jayin Ton <tonjayin@gmail.com>
2016-10-28 18:04:07 +0800

    Member/Field/index: #41模型管理UI调整

--- app/Application/Member/View/Field/index.php
+++ app/Application/Member/View/Field/index.php
@@ -15,41 +15,41 @@
         <thead>
           <tr>
             <td width="70" align="center">排序</td>
-            <td width="90">字段名</td>
-            <td width="100">别名</td>
+            <td width="180">字段名</td>
+            <td width="180">别名</td>
             <td width="100" align="center">类型</td>
             <td width="50" align="center">必填</td>
             <td width="50" align="center">搜索</td>
             <td width="50" align="center">排序</td>
-            <td  align="center">管理操作</td>
+            <td align="left">管理操作</td>
           </tr>
         </thead>
         <tbody class="td-line">
         <volist name="data" id="vo">
           <tr>
             <td align='center' width='70'><input name='listorders[{$vo.fieldid}]' type='text' size='3' value='{$vo.listorder}' class='input'></td>
-            <td width='90'>{$vo.field}</td>
-            <td width="100">{$vo.name}</td>
+            <td width='180'>{$vo.field}</td>
+            <td width="180">{$vo.name}</td>
             <td width="100" align='center'>{$vo.formtype}</td>
             <td width="50" align='center'><font color="red"><if condition="$vo['minlength'] eq 1">√<else /> ╳</if></font></td>
             <td width="50" align='center'><font color="red"><if condition="$vo['issearch'] eq 1">√<else /> ╳</if></font></td>
             <td width="50" align='center'><font color="blue"><if condition="$vo['isorder'] eq 1">√<else /> ╳</if></font></td>
-            <td align='center'>
-            <a href="{:U("Field/edit",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid']))}">修改</a> | 
-            <if condition=" in_array($vo['field'],$forbid_fields) ">
-            <font color="#BEBEBE"> 禁用 </font>|
-            <else />
-                 <if condition=" $vo['disabled'] eq 0 ">
-                 <a href="{:U("Field/disabled",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid'],"disabled"=>0))}">禁用</a> |
-                 <else />
-                 <a href="{:U("Field/disabled",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid'],"disabled"=>1))}"><font color="#FF0000">启用</font></a> |
-                 </if>
-            </if>
-            <if condition=" in_array($vo['field'],$forbid_delete) ">
-            <font color="#BEBEBE"> 删除</font>
-            <else />
-            <a href="javascript:confirmurl('{:U("Field/delete",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid']))}','确认要删除 『 {$vo.name} 』 吗？')">删除</a>
-            </if>
+            <td align='left'>
+                <a href="{:U("Field/edit",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid']))}">修改</a> |
+                <if condition=" in_array($vo['field'],$forbid_fields) ">
+                    <font color="#BEBEBE"> 禁用 </font>|
+                <else />
+                     <if condition=" $vo['disabled'] eq 0 ">
+                        <a href="{:U("Field/disabled",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid'],"disabled"=>0))}">禁用</a> |
+                     <else />
+                        <a href="{:U("Field/disabled",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid'],"disabled"=>1))}"><font color="#FF0000">启用</font></a> |
+                     </if>
+                </if>
+                <if condition=" in_array($vo['field'],$forbid_delete) ">
+                    <font color="#BEBEBE"> 删除</font>
+                <else />
+                    <a href="javascript:confirmurl('{:U("Field/delete",array("fieldid"=>$vo['fieldid'],"modelid"=>$vo['modelid']))}','确认要删除 『 {$vo.name} 』 吗？')">删除</a>
+                </if>
             </td>
           </tr>
         </volist>
