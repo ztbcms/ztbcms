a2e60a5e3592a6e87f3004791d1d26f451bd2169
Jayin Ton <tonjayin@gmail.com>
2016-11-06 10:32:39 +0800

    Realese 3.0.4.0

--- app/Common/Conf/version.php
+++ app/Common/Conf/version.php
@@ -6,6 +6,6 @@
 
 return array(
 	"CMS_APPNAME" => "ZtbCMS内容管理系统", //产品名称
-	"CMS_BUILD" => 201611030921, //产品流水号
-	"CMS_VERSION" => "3.0.3.0", //产品版本号
+	"CMS_BUILD" => 201611061032, //产品流水号
+	"CMS_VERSION" => "3.0.4.0", //产品版本号
 );
