c3a743f1e837a69677fe30c24572b636dcfaa979
Jayin Ton <tonjayin@gmail.com>
2016-12-14 00:58:34 +0800

    module/transport: add sql

---
+++ app/Application/Transport/Uninstall/transport.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `cms_transport_condition`;
+DROP TABLE IF EXISTS `cms_transport_field`;
+DROP TABLE IF EXISTS `cms_transport_task`;
+DROP TABLE IF EXISTS `cms_transport_task_log`;
