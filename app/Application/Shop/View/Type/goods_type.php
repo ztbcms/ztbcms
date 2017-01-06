04968832ac81ce87250c445ec12313f277f38348
Jayin Ton <tonjayin@gmail.com>
2017-01-06 18:14:41 +0800

    module/shop: fix draft

--- app/Application/Shop/View/Type/goods_type.php
+++ app/Application/Shop/View/Type/goods_type.php
@@ -9,7 +9,7 @@
             </div>
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title"><i class="fa fa-list"></i> 订单详情</h3>
+                    <h3 class="panel-title"><i class="fa fa-list"></i> 商品类型</h3>
                 </div>
                 <div class="panel-body">
                     <ul class="nav nav-tabs">
