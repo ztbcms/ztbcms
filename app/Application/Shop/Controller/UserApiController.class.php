5262f6067fcd3518e9c5cf0f38f59ca324a82a79
zhlhuang <364626853@qq.com>
2017-01-06 20:11:40 +0800

    #101  用户相关操作的返回格式优化

--- app/Application/Shop/Controller/UserApiController.class.php
+++ app/Application/Shop/Controller/UserApiController.class.php
@@ -1,12 +1,13 @@
 <?php
 namespace Shop\Controller;
+
 use Shop\Logic\ShopUsersLogic;
 use Shop\Service\UserService;
 
 class UserApiController extends BaseController {
     public function _initialize() {
         parent::_initialize();
-    
+
     }
     /**
      * 获取登录用户信息
@@ -15,11 +16,12 @@ class UserApiController extends BaseController {
         $userinfo = service("Passport")->getInfo();
         $shop_user = M('ShopUsers')->find($userinfo['userid']);
         if ($userinfo && $shop_user) {
-            exit(json_encode(array('status' => 1, 'data' => $shop_user, 'msg' => 'ok')));
+            $this->success($shop_user);
         } else {
-            exit(json_encode(array('status' => -500, 'msg' => '没有登录')));
+            $this->error('没有登录', -500);
         }
     }
+
     /**
      * 用户登录api
      */
@@ -40,94 +42,106 @@ class UserApiController extends BaseController {
             setcookie('cn', 0, time() - 3600, '/');
             unset($res['password']);
             $this->success($res);
-        }else{
+        } else {
             $this->error($user_service->get_err_msg());
         }
     }
+
     public function register() {
         //验证码检验
         $username = I('post.username', '');
         $password = I('post.password', '');
         $password2 = I('post.password2', '');
-        $user_service=new UserService();
+        $user_service = new UserService();
         $res = $user_service->register($username, $password, $password2);
         if ($res) {
             session('user', $res);
             $this->success($res);
-        }else{
+        } else {
             $this->error($user_service->get_err_msg());
         }
     }
-     /*
+
+    /**
      * 用户地址列表
      */
-    public function address_list(){
-        $address_lists = M('UserAddress')->where(array('userid'=>$this->userid))->select();
-        $list=array();
-        foreach($address_lists as $key=>$value){
-            $value['province_name']=getRegionName($value['province'],1);
-            $value['city_name']=getRegionName($value['city'],2);
-            $value['district_name']=getRegionName($value['district'],3);
-            $list[]=$value;
+    public function address_list() {
+        $address_lists = M('UserAddress')->where(array('userid' => $this->userid))->select();
+        $list = [];
+        foreach ($address_lists as $key => $value) {
+            $value['province_name'] = getRegionName($value['province'], 1);
+            $value['city_name'] = getRegionName($value['city'], 2);
+            $value['district_name'] = getRegionName($value['district'], 3);
+            $list[] = $value;
         }
-        $resulet=array('status'=>1,'data'=>$list);
-        exit(json_encode($resulet));
+        $this->success($list);
     }
-      /*
+
+    /**
      * 添加地址
      */
-    public function add_address(){
-        if(IS_POST){
+    public function add_address() {
+        if (IS_POST) {
             $logic = new ShopUsersLogic();
-            $data = $logic->add_address($this->userid,0,I('post.'));
-            exit(json_encode($data)); 
+            $data = $logic->add_address($this->userid, 0, I('post.'));
+            $this->success($data);
+        } else {
+            $this->error('请求方法错误');
         }
     }
-     /*
+
+    /**
      * 编辑地址
      */
-    public function edit_address(){
-        if(IS_POST){
-            $id=I('post.id');
-            if(!$id){
-                exit(json_encode(array('status'=>-1,'msg'=>'参数错误')));
+    public function edit_address() {
+        if (IS_POST) {
+            $id = I('post.id');
+            if (!$id) {
+                exit(json_encode(array('status' => -1, 'msg' => '参数错误')));
             }
-            $post=I('post.');
+            $post = I('post.');
             unset($post['id']);
             $logic = new ShopUsersLogic();
-            $data = $logic->add_address($this->userid,$id,$post);
-            exit(json_encode($data)); 
+            $data = $logic->add_address($this->userid, $id, $post);
+            $this->success($data);
+        }else{
+            $this->error('请求方法错误');
         }
     }
-      /*
+
+    /**
      * 设置默认收货地址
      */
-    public function set_default(){
+    public function set_default() {
         $id = I('post.id');
-        M('UserAddress')->where(array('userid'=>$this->userid))->save(array('is_default'=>0));
-        $row = M('UserAddress')->where(array('userid'=>$this->userid,'address_id'=>$id))->save(array('is_default'=>1));
-        if(!$row)
-            $resulet=array('status'=>-1,'msg'=>'操作失败');
-        else
-            $resulet=array('status'=>1,'msg'=>'操作成功');
-        exit(json_encode($resulet));
+        M('UserAddress')->where(array('userid' => $this->userid))->save(array('is_default' => 0));
+        $row = M('UserAddress')->where(array(
+            'userid' => $this->userid,
+            'address_id' => $id
+        ))->save(array('is_default' => 1));
+        if (!$row) {
+            $this->error('操作失败');
+        } else {
+            $this->success('操作成功');
+        }
     }
-      /*
+
+    /**
      * 地址删除
      */
-    public function del_address(){
+    public function del_address() {
         $id = I('post.id');
         $address = M('UserAddress')->where("address_id = $id")->find();
-        $row = M('UserAddress')->where(array('userid'=>$this->userid,'address_id'=>$id))->delete();                
+        $row = M('UserAddress')->where(array('userid' => $this->userid, 'address_id' => $id))->delete();
         // 如果删除的是默认收货地址 则要把第一个地址设置为默认收货地址
-        if($address['is_default'] == 1){
-            $address2 = M('UserAddress')->where("userid = {$this->userid}")->find();            
-            $address2 && M('UserAddress')->where("address_id = {$address2['address_id']}")->save(array('is_default'=>1));
-        }        
-        if(!$row)
-            $resulet=array('status'=>-1,'msg'=>'操作失败');
-        else
-            $resulet=array('status'=>1,'msg'=>'操作成功');
-        exit(json_encode($resulet));
+        if ($address['is_default'] == 1) {
+            $address2 = M('UserAddress')->where("userid = {$this->userid}")->find();
+            $address2 && M('UserAddress')->where("address_id = {$address2['address_id']}")->save(array('is_default' => 1));
+        }
+        if (!$row) {
+            $this->error('操作失败');
+        } else {
+            $this->success('操作成功');
+        }
     }
 }
