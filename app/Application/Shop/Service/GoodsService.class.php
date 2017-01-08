ee6815fdae36d2629b562a3bf7ccba0880fefb73
zhlhuang <364626853@qq.com>
2017-01-08 16:47:22 +0800

    #101 获取商品详情api

--- app/Application/Shop/Service/GoodsService.class.php
+++ app/Application/Shop/Service/GoodsService.class.php
@@ -27,5 +27,31 @@ class GoodsService extends BaseService {
 
         return $res;
     }
+
+    /**
+     * 获取相片所属的规格信息
+     * @param $goods_id
+     * @return array
+     */
+
+    static function get_spec($goods_id) {
+        //商品规格 价钱 库存表 找出 所有 规格项id
+        $keys = M('SpecGoodsPrice')->where("goods_id = $goods_id")->getField("GROUP_CONCAT(`key` SEPARATOR '_') ");
+        $filter_spec = array();
+        if ($keys) {
+            $specImage = M('SpecImage')->where("goods_id = $goods_id and src != '' ")->getField("spec_image_id,src");// 规格对应的 图片表， 例如颜色
+            $keys = str_replace('_', ',', $keys);
+            $sql = "SELECT a.name,a.order,b.* FROM __PREFIX__spec AS a INNER JOIN __PREFIX__spec_item AS b ON a.id = b.spec_id WHERE b.id IN($keys) ORDER BY b.id";
+            $filter_spec2 = M()->query($sql);
+            foreach ($filter_spec2 as $key => $val) {
+                $filter_spec[$val['name']][] = array(
+                    'item_id' => $val['id'],
+                    'item' => $val['item'],
+                    'src' => $specImage[$val['id']],
+                );
+            }
+        }
+        return $filter_spec;
+    }
 }
 
