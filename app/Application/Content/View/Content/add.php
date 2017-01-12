f4898dc3117d7b556fe3f2dcfabd9cf567feab29
Jayin Ton <tonjayin@gmail.com>
2017-01-12 17:21:01 +0800

    Update draft

--- app/Application/Content/View/Content/add.php
+++ app/Application/Content/View/Content/add.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<title>添加新文章 - 系统后台 - {$Config.sitename} - by ZtbCMS</title>
+<title>添加 - 系统后台 - {$Config.sitename} - by ZtbCMS</title>
 <Admintemplate file="Admin/Common/Cssjs"/>
 <script type="text/javascript">
     var catid = "{$catid}";
