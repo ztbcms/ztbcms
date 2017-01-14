c78986292d30d7c49e74fbe41148031ab610601f
Jayin Ton <tonjayin@gmail.com>
2017-01-14 21:23:12 +0800

    分离Wap模块

--- app/Application/Wap/Install/Template/Wap/Page/page.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('CMS_VERSION')) exit(); ?>
-<template file="Wap/header.php"/>
-<div style="width:100%; height:200px; background:#F9C">
-标题：{$title}<br/>
-关键字：{$keywords}<br/>
-内容：{$content}<br/>
-<template file="Wap/footer.php"/>
