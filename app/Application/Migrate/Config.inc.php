1b68755fa47f26ccd2b47910245ef26d6ad89070
Jayin Ton <tonjayin@gmail.com>
2017-02-03 21:33:46 +0800

    Core: 添加默认并缺省的模块：Testing、Migrate

---
+++ app/Application/Migrate/Config.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * 模块安装，基本配置
+ */
+return array(
+    //模块名称
+    'modulename' => '数据迁移',
+    //图标
+    'icon' => 'https://dn-coding-net-production-pp.qbox.me/e57af720-f26c-4f3b-90b9-88241b680b7b.png',
+    //模块简介
+    'introduce' => '数据修正、处理',
+    //模块介绍地址
+    'address' => 'http://www.ztbcms.com',
+    //模块作者
+    'author' => 'ZTBCMS',
+    //作者地址
+    'authorsite' => 'http://www.ztbcms.com',
+    //作者邮箱
+    'authoremail' => 'admin@ztbcms.com',
+    //版本号，请不要带除数字外的其他字符
+    'version' => '1.0.0.0',
+    //适配最低版本，
+    'adaptation' => '3.0.0.0',
+    //签名
+    'sign' => '2bd344ce01877125fb35f5a1504d5e58',
+    //依赖模块
+    'depend' => array(),
+    //注册缓存
+    'cache' => array(),
+);
