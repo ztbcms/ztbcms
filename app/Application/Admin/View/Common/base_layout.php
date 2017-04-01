0143788ca106b3cd41ce859baa12fee988e3a6e4
Jayin Ton <tonjayin@gmail.com>
2017-04-01 22:13:39 +0800

    添加vue.js 到默认的base_layout

---
+++ app/Application/Admin/View/Common/base_layout.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>系统后台 - {$Config.sitename}</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="{$config_siteurl}statics/admin/theme/adminlte/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link href="{$config_siteurl}statics/admin/theme/adminlte/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{$config_siteurl}statics/admin/theme/adminlte/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE 皮肤. 可以从/statics/admin/theme/adminlte/dist/css/skins/目录中选择其中一个 -->
+    <link rel="stylesheet" href="{$config_siteurl}statics/admin/theme/adminlte/dist/css/skins/skin-blue.css">
+
+    <!-- jQuery 2.2.0 -->
+    <script src="{$config_siteurl}statics/admin/theme/adminlte/plugins/jQuery/jQuery-2.2.0.min.js"></script>
+
+    <!-- Bootstrap 3.3.6 -->
+    <script src="{$config_siteurl}statics/admin/theme/adminlte/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- vue.js -->
+    <script src="{$config_siteurl}statics/js/vue/vue.js"></script>
+</head>
+<body class="hold-transition skin-blue sidebar-mini fixed" style="height: 100%;">
+
+<block name="content">
+
+</block>
+
+</body>
+
+</html>
