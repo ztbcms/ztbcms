d2fee3f9e794035fb59d2ed5b01762616649034e
Jayin Ton <tonjayin@gmail.com>
2017-04-05 12:29:52 +0800

    Release v3.6.0.10

--- app/Common/Conf/version.php
+++ app/Common/Conf/version.php
@@ -6,6 +6,6 @@
 
 return array(
 	"CMS_APPNAME" => "ZTBCMS内容管理系统", //产品名称
-    "CMS_BUILD" => 201703061755, //产品流水号
-    "CMS_VERSION" => "3.6.0.9", //产品版本号
+    "CMS_BUILD" => 201704051227, //产品流水号
+    "CMS_VERSION" => "3.6.0.10", //产品版本号
 );
