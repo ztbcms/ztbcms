9f5fcbb7ac6bba3efb48e5c23b93bce146356925
Jayin Ton <tonjayin@gmail.com>
2017-07-21 15:58:26 +0800

    Hook:新增 用户注册成功 member_register

---
+++ app/Application/Member/Behavior/MemberRegisterBehavior.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * author: Jayin <tonjayin@gmail.com>
+ */
+
+namespace Member\Behavior;
+
+use Common\Behavior\BaseBehavior;
+
+/**
+ * 用户完成注册回调
+ */
+class MemberRegisterBehavior extends BaseBehavior {
+
+    public function run(&$param) {
+        parent::run($param);
+    }
+
+}
