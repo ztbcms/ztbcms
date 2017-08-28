63575eaebf829248c6c87bcc4b65063f416a278a
Jayin Ton <tonjayin@gmail.com>
2017-08-28 17:16:35 +0800

    admin: 后台管理用户新增phone字段

--- app/Application/Install/Data/cms.sql
+++ app/Application/Install/Data/cms.sql
@@ -888,6 +888,7 @@ CREATE TABLE `cms_user` (
   `role_id` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT '对应角色ID',
   `info` text COMMENT '信息',
   `avatar` varchar(256) NOT NULL DEFAULT '' COMMENT '头像链接',
+  `phone` varchar(32) NOT NULL DEFAULT '' COMMENT '手机号码',
   PRIMARY KEY (`id`),
   UNIQUE KEY `account` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户表';
