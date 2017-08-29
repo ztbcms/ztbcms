4700ad0471e6c3f7e5e08f8c529364fb09e42ac1
Jayin Ton <tonjayin@gmail.com>
2017-08-29 14:21:12 +0800

    提升 cache 目录的安全性 相关链接：http://www.thinkphp.cn/topic/51162.html

--- app/Core/Conf/convention.php
+++ app/Core/Conf/convention.php
@@ -76,7 +76,7 @@ return  array(
     'DATA_CACHE_PREFIX'     =>  '',     // 缓存前缀
     'DATA_CACHE_TYPE'       =>  'File',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
     'DATA_CACHE_PATH'       =>  TEMP_PATH,// 缓存路径设置 (仅对File方式缓存有效)
-    'DATA_CACHE_KEY'        =>  '',	// 缓存文件KEY (仅对File方式缓存有效)    
+    'DATA_CACHE_KEY'        =>  'ztbcms',	// 缓存文件KEY (仅对File方式缓存有效)
     'DATA_CACHE_SUBDIR'     =>  false,    // 使用子目录缓存 (自动根据缓存标识的哈希创建子目录)
     'DATA_PATH_LEVEL'       =>  1,        // 子目录缓存级别
 
