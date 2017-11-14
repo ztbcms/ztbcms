f07cbfb0bbcd170094820c04d23b53d98eec6576
Jayin Ton <tonjayin@gmail.com>
2017-11-14 12:53:23 +0800

    新增基类BaseRelationModel

---
+++ app/Common/Model/BaseRelationModel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * author: Jayin <tonjayin@gmail.com>
+ */
+
+namespace Common\Model;
+
+/**
+ * 关联模型基类
+ *
+ * @property mixed ENABLE_FIELDS
+*/
+class BaseRelationModel extends RelationModel {
+
+
+}
