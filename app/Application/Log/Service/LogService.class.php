6d0533974e599ebd4ad1997e96b5f0f191e09c8a
Jayin Ton <tonjayin@gmail.com>
2018-10-08 16:34:01 +0800

    Module/Log: 日志列表默认显示最近 7 日

--- app/Application/Log/Service/LogService.class.php
+++ app/Application/Log/Service/LogService.class.php
@@ -40,8 +40,8 @@ class LogService extends BaseService{
     /**
      * 根据传入的：类别关键字，起始日期，结束日期，指定页码，指定记录数获取数据
      * @param string $category      类别关键词
-     * @param string $start_date    起始日期
-     * @param string $end_date      结束日期
+     * @param string $start_date    起始日期 ，格式：2018-01-01
+     * @param string $end_date      结束日期 ，格式：2018-01-01
      * @param int    $page          指定的分页页码
      * @param int    $limit         指定显示的记录条数
      * @param string $message       指定
@@ -56,29 +56,34 @@ class LogService extends BaseService{
         if (!empty($category)) {
             $where['category'] = array('LIKE', '%'.$category.'%');
         }
-        if (!empty($start_date) && !empty($end_date)) {
+        //
+        if(!empty($start_date)){
             //将输入的起始和结束时间转换成时间戳
             $start_date = strtotime($start_date);
+            $where['inputtime'] = array('EGT', $start_date);
+        }
+
+        if(!empty($end_date)){
             //这里是下面的计算是因为单单转换"结束日期"为时间戳的话，并不会包括"结束日期"的那一天
             $end_date = strtotime($end_date) + 24 * 60 * 60 - 1;
-            //'EGT'是大于，'ELT'是小于
-            $where['inputtime'] = array(array('EGT', $start_date), array('ELT', $end_date), 'AND');
+            if(isset($where['inputtime'])){
+                $where['inputtime'] = array($where['inputtime'], array('ELT', $end_date), 'AND');
+            }else{
+                $where['inputtime'] = array('ELT', $end_date);
+            }
+
         }
-        else {
-            //如果是起始日期为空的话，那么给其默认为今天
-            !empty($start_date) ? : $start_date = time();
-            !empty($end_date) ? : $end_date = time() + 24 * 60 * 60 -1;
-        }
+
         if (!empty($message)) {
             $where['message'] = array('LIKE', '%' . $message . '%');
         }
 
         //获取总记录数
-        $count = D('Log/Log')->where($where)->count();
+        $count = $db->where($where)->count();
         //总页数
         $total_page = ceil($count / $limit);
         //获取到的分页数据
-        $Logs = D('Log/Log')->where($where)->page($page)->limit($limit)->order(array("id" => "desc"))->select();
+        $Logs = $db->where($where)->page($page)->limit($limit)->order(array("id" => "desc"))->select();
         $data = [
             'items' => $Logs,
             'page' => $page,
