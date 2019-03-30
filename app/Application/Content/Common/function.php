a30c43b0b8d9536e8ae1d230d86641f5448f9557
zhlhuang <364626853@qq.com>
2019-03-30 17:41:35 +0800

    #329 调整日期类型错误，获取box选择的名称

--- app/Application/Content/Common/function.php
+++ app/Application/Content/Common/function.php
@@ -3,3 +3,13 @@
 // +----------------------------------------------------------------------
 // | 内容模块自定义函数
 // +----------------------------------------------------------------------
+
+function getBoxOptionsName($value, $catid, $field) {
+    $category = getCategory($catid);
+
+    $model = getModel($category['modelid']);
+    $tableName = $model['tablename'] . '_box_' . $field;
+    $box = M($tableName)->where(['value' => $value])->find();
+
+    return $box['label'];
+}
