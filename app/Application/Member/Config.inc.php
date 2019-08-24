f2e614ce6530b20fd4e5ea2bf9947c0cf4701940
Jayin Ton <tonjayin@gmail.com>
2019-08-24 17:19:47 +0800

    Module/Member: v1.0.2.1

--- app/Application/Member/Config.inc.php
+++ app/Application/Member/Config.inc.php
@@ -19,7 +19,7 @@ return array(
     //作者邮箱
     'authoremail' => 'admin@ztbcms.com',
     //版本号，请不要带除数字外的其他字符
-    'version' => '1.0.2.0',
+    'version' => '1.0.2.1',
     //适配最低版本，
     'adaptation' => '3.0.0.0',
     //签名
