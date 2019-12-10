7329d4c8f1338b77db098bd34fa62ec6c6bc00c9
Jayin Ton <tonjayin@gmail.com>
2019-12-11 00:57:08 +0800

    后台登录：数据传输采用128位加密技术，保障您的信息安全

--- app/Application/Admin/View/Common/Js.php
+++ app/Application/Admin/View/Common/Js.php
@@ -13,6 +13,12 @@ var GV = {
 <script src="{$config_siteurl}statics/admin/layer/layer.js"></script>
 <!--  ztbcms工具类(必须在vue-commonn 前加载)  -->
 <script src="{$config_siteurl}statics/js/ztbcms/ztbcms.js"></script>
+
+<!--cryptojs 请务必再ztbcms.js后加在-->
+<script src="{$config_siteurl}statics/admin/cryptojs/crypto-js.js"></script>
+<script src="{$config_siteurl}statics/js/ztbcms/ztbcms_crypt.js"></script>
+<!--cryptojs-->
+
 <!-- vue.js -->
 <script src="{$config_siteurl}statics/js/vue/vue.js"></script>
 <script src="{$config_siteurl}statics/js/vue/vue-common.js"></script>
