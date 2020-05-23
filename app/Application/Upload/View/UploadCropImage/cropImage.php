a0fd13c572709c8eae3f3b42523ae18db6e61fa2
boringiii <646847330@qq.com>
2020-05-23 13:56:33 +0800

    update

--- app/Application/Upload/View/UploadCropImage/cropImage.php
+++ app/Application/Upload/View/UploadCropImage/cropImage.php
@@ -22,9 +22,8 @@
             max-width: 200px;
         }
         .img-container{
-            width: 100%;
+            width: 98%;
             height: 500px;
-            margin: 10px;
         }
     </style>
 </block>
@@ -33,37 +32,42 @@
     <!-- Content -->
     <div class="container" id="app" v-cloak>
         <el-row>
-            <el-col :span="16">
+            <el-col :span="16" style="margin:10px;">
                 <div class="img-container">
-                    <img src="" alt="Picture" ref="image" >
+                    <img src="{$config_siteurl}statics/admin/cropper/white.png" alt="" ref="image">
                 </div>
             </el-col>
-            <el-col :span="6" style="margin: 10px;">
+            <el-col :span="6">
                 <div>图片预览</div>
-                <div class="preview" style="margin-bottom: 10px;"></div>
+                <div class="preview" style="margin: 10px 0;"></div>
                 <div>
                     <img :src="save_img_url" alt="" class="save_img_url" style="display:none;">
                 </div>
                 <div>
                     <div>裁剪框宽高比：</div>
+                    <div class="">
+                        <el-input placeholder="" v-model="input_width" style="margin: 10px 0;width: 263px;" @input="listenInputWidth">
+                            <template slot="prepend">宽</template>
+                        </el-input>
+                        <br>
+                        <el-input placeholder="" v-model="input_height" style="margin-bottom: 10px;width: 263px;" @input="listenInputHeight">
+                            <template slot="prepend">高</template>
+                        </el-input>
+                    </div>
                     <el-button-group>
-                        <el-button type="primary" size="medium" @click="setAspectRatio(16 / 9)">
+                        <el-button type="primary" size="medium" @click="setAspectRatio(16,9)">
                           16:9
                         </el-button>
 
-                        <el-button type="primary" size="medium" @click="setAspectRatio(4 / 3)">
+                        <el-button type="primary" size="medium" @click="setAspectRatio(4,3)">
                           4:3
                         </el-button>
 
-                        <el-button type="primary" size="medium" @click="setAspectRatio( 2 / 3)">
-                          2:3
-                        </el-button>
-
-                        <el-button type="primary" size="medium" @click="setAspectRatio(1 / 1)">
+                        <el-button type="primary" size="medium" @click="setAspectRatio(1,1)">
                           1:1
                         </el-button>
 
-                        <el-button type="primary" size="medium" @click="setAspectRatio(NaN)">
+                        <el-button type="primary" size="medium" @click="setAspectRatio(NaN,NaN)">
                           自由
                         </el-button>
                     </el-button-group>
@@ -94,8 +98,7 @@
                 </el-button-group>
                 <label class="selectImage" for="inputImage">
                     <input class="sr-only" id="inputImage" name="file" type="file" accept="image/*" style="display:none;"
-                           v-on:change="changeImg"
-                           >
+                           v-on:change="changeImg">
                     <span>
                         <i class="el-icon-upload2"></i>
                         <span>选择图片</span>
@@ -117,8 +120,9 @@
                 el:"#app",
                 data:{
                     img_url: "",
-                    img_index:"",
-                    save_img_url:""
+                    save_img_url:"",
+                    input_width:"",
+                    input_height:"",
                 },
                 methods:{
                     //初始化
@@ -132,6 +136,13 @@
                             autoCropArea: 0.6,      //设置裁剪区域占图片的大小 值为 0-1 默认 0.8 表示 80%的区域
                             zoomOnWheel: false,     //是否可以通过鼠标滚轮缩放图片 默认true
                         })
+                    },
+                    //监听输入 宽
+                    listenInputWidth(){
+                        this.setAspectRatio(this.input_width,this.input_height)
+                    },
+                    listenInputHeight(){
+                        this.setAspectRatio(this.input_width,this.input_height)
                     },
                     //放大 缩小
                     Zoom(option){
@@ -154,9 +165,8 @@
                         var event = document.createEvent('CustomEvent');
                         event.initCustomEvent('ZTBCMS_IMAGE_CROP_FILE', true, true, {
                             img_base64: this.save_img_url,
-                            img_index:  this.img_index
                         });
-                        window.parent.dispatchEvent(event) //触发
+                        window.parent.dispatchEvent(event); //触发
                         this.closePanel();
                     },
                     //关闭窗口
@@ -168,7 +178,10 @@
                         }
                     },
                     //设置裁剪的宽高比
-                    setAspectRatio(option){
+                    setAspectRatio(width,height){
+                        var option = width / height;
+                        this.input_width  = width;
+                        this.input_height = height;
                         this.myCropper.setAspectRatio(option)
                     },
                     //更换图片
@@ -190,13 +203,15 @@
                 },
                 mounted: function () {
                     this.init(); // 初始化
-                    // 若有传入则更换url，否则为空
+                    // 传入指定图片url
                     this.img_url = this.getUrlQuery('url') || "";
-                    // 若是编辑模式，则将返回图片index
-                    this.img_index = this.getUrlQuery('img_index') || "";
                     if(this.img_url != ""){
                         this.myCropper.replace(this.img_url)
                     }
+                    // 设置宽高比
+                    var width = this.getUrlQuery('width') || "";
+                        height = this.getUrlQuery('height') || "";
+                    this.setAspectRatio(width,height)
                 }
             })
         })
