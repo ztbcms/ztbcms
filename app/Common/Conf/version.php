9b8f6496440fdf21f294f2237a1045f5321d2f51
Jayin Ton <tonjayin@gmail.com>
2020-08-14 16:05:50 +0800

    v3.8.10.3

--- app/Common/Conf/version.php
+++ app/Common/Conf/version.php
@@ -5,6 +5,6 @@
 return array(
 	"CMS_APPNAME" => "ZTBCMS内容管理系统", //CMS名称
     "CMS_BUILD" => '202007201740', //CMS流水号
-    "CMS_VERSION" => "3.8.10.2", //CMS版本号
+    "CMS_VERSION" => "3.8.10.3", //CMS版本号
     "APPLIATION_VERSION" => "0.0.0", //产品版本号
 );
