74dc3c2171c9536c943c6f4e9068501d0b195a9d
zhlhuang <364626853@qq.com>
2020-09-07 11:49:10 +0800

    网站配置Model

---
+++ tp6/app/common/model/ConfigModel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhlhuang
+ * Date: 2020-09-07
+ * Time: 09:36.
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+/**
+ *  网站配置
+ * Class ConfigModel
+ * @package app\common\model
+ */
+class ConfigModel extends Model
+{
+    protected $name = 'config';
+}
