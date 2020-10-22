ad2a3ac5d5d2344b4a3729b6dcec3b87bdf2ec37
Jayin Ton <tonjayin@gmail.com>
2020-10-22 11:41:58 +0800

    新增配置缓存

--- tp6/app/admin/service/AdminConfigService.php
+++ tp6/app/admin/service/AdminConfigService.php
@@ -8,6 +8,7 @@ namespace app\admin\service;
 
 
 use app\common\service\BaseService;
+use think\facade\Cache;
 use think\facade\Db;
 
 /**
@@ -18,15 +19,32 @@ use think\facade\Db;
  */
 class AdminConfigService extends BaseService
 {
+    /**
+     * 缓存Tag名称
+     */
+    const CacheTagName = 'AdminConfig';
+
     static function getInstance()
     {
         return new AdminConfigService();
     }
 
     /**
+     * 生成缓存key
+     *
+     * @param  string  $key
+     *
+     * @return string
+     */
+    private function _makeAdminConfigCacheKey($key = '')
+    {
+        return 'AdminConfig_'.$key;
+    }
+
+    /**
      * 获取配置
      *
-     * @param  string  $key 配置的key,为空的时候返回全部
+     * @param  string  $key  配置的key,为空的时候返回全部
      *
      * @return array
      * @throws \think\db\exception\DataNotFoundException
@@ -35,45 +53,72 @@ class AdminConfigService extends BaseService
      */
     function getConfig($key = '')
     {
-        //TODO 缓存config
+        $cache_key = $this->_makeAdminConfigCacheKey($key);
+        $cache_data = Cache::get($cache_key);
+        if (!empty($cache_data)) {
+            return self::createReturn(true, $cache_data);
+        }
+
         $configList = Db::name('config')->field('varname,value')->select()->toArray();
         $config = [];
         foreach ($configList as $i => $v) {
             $config[$v['varname']] = $v['value'];
         }
         if (!empty($key)) {
+            Cache::tag(self::CacheTagName)->set($cache_key, $config[$key]);
             return self::createReturn(true, $config[$key]);
         }
+        Cache::tag(self::CacheTagName)->set($cache_key, $config);
         return self::createReturn(true, $config);
     }
 
     /**
      * 更新配置项
      *
-     * @param  array $keyValue
+     * @param  array  $keyValue
      *
      * @return array
      * @throws \think\db\concern\PDOException
      * @throws \think\db\exception\DbException
      */
-    function updateConfig(array $keyValue = []){
-        Db::startTrans();
-        foreach ($keyValue as $key => $value){
-            Db::name('config')->where('varname', $key)->update([
+    function updateConfig(array $keyValue = [])
+    {
+        foreach ($keyValue as $key => $value) {
+            $res = Db::name('config')->where('varname', $key)->update([
                 'value' => $value
             ]);
-        }
-        Db::commit();
+            if ($res) {
+                $cache_key = $this->_makeAdminConfigCacheKey($key);
+                Cache::tag(self::CacheTagName)->set($cache_key, $value);
+            }
 
+        }
         return self::createReturn(true, null, '配置更新完成');
     }
 
-    function getConfigFielList(){
+    /**
+     * 获取配置字段列表
+     *
+     * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    function getConfigFielList()
+    {
         $lists = Db::name('config_field')->select()->toArray();
-        foreach ($lists as &$item){
+        foreach ($lists as &$item) {
             $item['setting'] = unserialize($item['setting']);
         }
 
         return self::createReturn(true, $lists);
+    }
+
+    /**
+     * 清理全部配置缓存
+     */
+    function clearConfigCache()
+    {
+        Cache::tag(self::CacheTagName)->clear();
     }
 }
