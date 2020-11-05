c3f35b08f0ea93cc6ace5cb05ffe5bf5c91765ab
cycle_3 <953006367@qq.com>
2020-11-05 13:48:46 +0800

    修复消息模块安装少表bug  新增删除日志的天数控制

--- tp6/app/common/uninstall/Common.sql
+++ tp6/app/common/uninstall/Common.sql
@@ -10,3 +10,10 @@ DROP TABLE IF EXISTS `cms_tp6_cron_config`;
 -- 消息模块
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_tp6_message_msg`;
+DROP TABLE IF EXISTS `cms_tp6_message_send_log`;
+
+-- ----------------------------
+-- 消息模块
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_tp6_attachment_group`;
+DROP TABLE IF EXISTS `cms_tp6_attachment`;
