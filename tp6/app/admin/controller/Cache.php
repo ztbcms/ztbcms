c9e47fec48b46b43d6eba2f34a75eb581dee93d9
cycle_3 <953006367@qq.com>
2020-11-05 17:27:48 +0800

    迁移常用的调用方法

--- tp6/app/admin/controller/Cache.php
+++ tp6/app/admin/controller/Cache.php
@@ -10,6 +10,7 @@ namespace app\admin\controller;
 use app\common\controller\AdminController;
 use think\facade\App;
 use think\facade\View;
+use app\admin\libs\helper\FileHelper;
 
 class Cache extends AdminController
 {
@@ -27,7 +28,7 @@ class Cache extends AdminController
             $path = App::getRootPath() . 'runtime\\';
             if (file_exists($path)) {
                 //判断是否存在缓存的页面文件
-                $this->deldir($path);
+                (new FileHelper)->deldir($path);
             }
             return json(self::createReturn(true, '', '清除成功'));
         } else if ($type == 'template') {
@@ -38,7 +39,7 @@ class Cache extends AdminController
                 if ($v != '.' && $v != '..') {
                     if (file_exists($path . '\\' . $v . '\temp')) {
                         //判断是否存在缓存的页面文件
-                        $this->deldir($path . '\\' . $v . '\temp\\');
+                        (new FileHelper)->deldir($path . '\\' . $v . '\temp\\');
                     }
                 }
             }
@@ -51,7 +52,7 @@ class Cache extends AdminController
                 if ($v != '.' && $v != '..') {
                     if (file_exists($path . '\\' . $v . '\log')) {
                         //判断是否存在缓存的页面文件
-                        $this->deldir($path . '\\' . $v . '\log\\');
+                        (new FileHelper)->deldir($path . '\\' . $v . '\log\\');
                     }
                 }
             }
@@ -61,34 +62,6 @@ class Cache extends AdminController
         }
     }
 
-
-    /**
-     * 删除文件
-     * @param $path
-     */
-    private function deldir($path)
-    {
-        //如果是目录则继续
-        if (is_dir($path)) {
-            //扫描一个文件夹内的所有文件夹和文件并返回数组
-            $p = scandir($path);
-            foreach ($p as $val) {
-                //排除目录中的.和..
-                if ($val != "." && $val != "..") {
-                    //如果是目录则递归子目录，继续操作
-                    if (is_dir($path . $val)) {
-                        //子目录中操作删除文件夹和文件
-                        $this->deldir($path . $val . '/');
-                        //目录清空后删除空文件夹
-                        @rmdir($path . $val . '/');
-                    } else {
-                        //如果是文件直接删除
-                        unlink($path . $val);
-                    }
-                }
-            }
-        }
-    }
 
 
 }
---
+++ tp6/app/admin/libs/helper/FileHelper.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * User: cycle_3
+ * Date: 2020/11/5
+ * Time: 17:23
+ */
+
+namespace app\admin\libs\helper;
+
+use app\common\service\BaseService;
+
+/**
+ * 文件辅助工具
+ * Class FileHelper
+ * @package app\admin\libs\helper
+ */
+class FileHelper extends BaseService
+{
+
+    /**
+     * 删除指定路径的文件
+     * @param $path
+     */
+    public function deldir($path)
+    {
+        //如果是目录则继续
+        if (is_dir($path)) {
+            //扫描一个文件夹内的所有文件夹和文件并返回数组
+            $p = scandir($path);
+            foreach ($p as $val) {
+                //排除目录中的.和..
+                if ($val != "." && $val != "..") {
+                    //如果是目录则递归子目录，继续操作
+                    if (is_dir($path . $val)) {
+                        //子目录中操作删除文件夹和文件
+                        $this->deldir($path . $val . '/');
+                        //目录清空后删除空文件夹
+                        @rmdir($path . $val . '/');
+                    } else {
+                        //如果是文件直接删除
+                        unlink($path . $val);
+                    }
+                }
+            }
+        }
+    }
+
+}
