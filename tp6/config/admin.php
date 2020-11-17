63d5cd3c67f940a8b2fb39a94d75936c8bb7371c
Jayin Ton <tonjayin@gmail.com>
2020-11-17 16:52:30 +0800

    remove config field

--- tp6/config/admin.php
+++ tp6/config/admin.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * author: Jayin <tonjayin@gmail.com>
+ * author: Jayin
  */
 
 // 管理后台相关信息
 return [
-    'admin_version'       => '4.0.0',
     "cms_name"            => "ZTBCMS内容管理系统", //CMS名称
     "cms_build"           => '202009031500', //CMS流水号
     "cms_version"         => "4.0.0", //CMS版本号
