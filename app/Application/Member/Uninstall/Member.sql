a13e16aaccbf061ca5918a4cd92ccac727c90ded
Jayin Ton <tonjayin@gmail.com>
2020-11-18 14:15:38 +0800

    删除tp3内容

--- app/Application/Member/Uninstall/Member.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `cms_connect`;
-DROP TABLE IF EXISTS `cms_member`;
-DROP TABLE IF EXISTS `cms_member_favorite`;
-DROP TABLE IF EXISTS `cms_member_online`;
-DROP TABLE IF EXISTS `cms_member_content`;
-DROP TABLE IF EXISTS `cms_member_group`;
--- statics/admin/demo/elementui/font/README.md
+++
@@ -1,5 +0,0 @@
-# 说明
-
-这里采用的是免费字体--思源黑体简体。请务必注意文字版权问题。
-
-下载地址：https://source.typekit.com/source-han-serif/cn/
