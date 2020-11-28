07755972bb8996d260a90e5dabd4162064ad066e
cycle_3 <953006367@qq.com>
2020-11-28 10:48:12 +0800

    #new 添加公共返回的方法

--- tp6/app/common.php
+++ tp6/app/common.php
@@ -48,3 +48,28 @@ if (!function_exists('api_url')) {
         return build_url($string, $vars, false, true);
     }
 }
+
+if(!function_exists('createReturn')) {
+
+    /**
+     * 统一返回的格式
+     * @param $status
+     * @param array $data 返回的内容
+     * @param string $msg 提示的文案
+     * @param null $code 状态值
+     * @param string $url
+     * @return array
+     */
+    function createReturn($status, $data = [], $msg = '', $code = null, $url = '') {
+        //默认成功则为200 错误则为400
+        if(empty($code)) $code = $status ? 200 : 400;
+        return [
+            'status' => $status,
+            'code'   => $code,
+            'data'   => $data,
+            'msg'    => $msg,
+            'url'    => $url,
+        ];
+    }
+
+}
