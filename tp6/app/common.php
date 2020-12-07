cedcfd5adc56f8f7e2b6a39d52d1e7abcfa07cb4
Jayin Ton <tonjayin@gmail.com>
2020-12-07 09:10:59 +0800

    remove `urlx()`

--- tp6/app/common.php
+++ tp6/app/common.php
@@ -1,20 +1,6 @@
 <?php
 // 应用公共文件
 
-/**
- * 因为挂载cms下，默认、tp6有一个 /home 开头
- * @param $string
- * @param array $vars
- * @param bool $suffix
- * @param bool $domain
- * @return \think\route\Url
- * @deprecated
- */
-function urlx($string, array $vars = [], $suffix = true, $domain = false)
-{
-    return url('/home/' . $string, $vars, $suffix, $domain);
-}
-
 if (!function_exists('build_url')) {
     /**
      * 构建路由
