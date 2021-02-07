9c34f4cc63594eaec99b2fa990ce81124af576b3
Jayin Ton <tonjayin@gmail.com>
2021-02-07 10:52:18 +0800

    Update

--- tp6/config/session.php
+++ tp6/config/session.php
@@ -14,7 +14,7 @@ return [
     'name'           => 'PHPSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
-    // 驱动方式 支持file cache
+    // 驱动方式
     'type'           => 'cache',
     // 存储连接标识 当type使用cache的时候有效
     'store'          => null,
