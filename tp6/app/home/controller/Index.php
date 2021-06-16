af8582ce8356561433392d82d9573f4cdd74ac08
唐杰英 <tonjayin@gmail.com>
2021-06-17 01:02:36 +0800

    Accept Merge Request #536: (feature/v4.1.0-535 -> develop) Merge Request: 优化首页样式 /home/index/index （#535）
    Created By: @黄振炼
    Accepted By: @唐杰英
    URL: https://gzztb.coding.net/p/ztbcms/d/ztbcms/git/merge/536

--- tp6/app/home/controller/Index.php
+++ tp6/app/home/controller/Index.php
@@ -6,9 +6,18 @@ use app\BaseController;
 
 class Index extends BaseController
 {
+    /**
+     * 首页
+     *
+     * @return \think\response\View
+     */
     public function index()
     {
-        return view('index');
+        $install_path = root_path('app/install');
+        $install_lock_path = $install_path.DIRECTORY_SEPARATOR.'install.lock';
+        $enable_install = file_exists($install_path) && !file_exists($install_lock_path);
+        return view('index', [
+            'enable_install' => $enable_install // 是否允许安装框架
+        ]);
     }
-
 }
