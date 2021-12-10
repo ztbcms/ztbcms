862415846dfa6ddd2072ae142d50d0e95ffa2b0f
Jayin Ton <tonjayin@gmail.com>
2021-12-10 10:04:42 +0800

    Fix: module/common uninstall sql

--- tp6/app/common/uninstall/Common.sql
+++ tp6/app/common/uninstall/Common.sql
@@ -7,13 +7,14 @@ DROP TABLE IF EXISTS `cms_tp6_cron_scheduling_log`;
 DROP TABLE IF EXISTS `cms_tp6_cron_config`;
 
 -- ----------------------------
--- 消息模块
+-- 消息
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_tp6_message_msg`;
 DROP TABLE IF EXISTS `cms_tp6_message_send_log`;
 
 -- ----------------------------
--- 消息模块
+-- 附件
 -- ----------------------------
-DROP TABLE IF EXISTS `cms_tp6_attachment_group`;
-DROP TABLE IF EXISTS `cms_tp6_attachment`;
+DROP TABLE IF EXISTS `cms_attachment_group`;
+DROP TABLE IF EXISTS `cms_attachment`;
+DROP TABLE IF EXISTS `cms_attachment_index`;
