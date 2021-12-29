c13fbb72f96f1d7807db42b9c701f99dbe83c7bd
Jayin Ton <tonjayin@gmail.com>
2021-12-29 17:10:44 +0800

    新增跨域中间件

--- tp6/app/common/middleware/Cors.php
+++ tp6/app/common/middleware/Cors.php
@@ -2,25 +2,38 @@
 
 namespace app\common\middleware;
 
-use app\admin\model\AdminUserModel;
-use app\admin\service\AdminUserService;
-use app\admin\service\RbacService;
-use think\facade\Config;
-use think\facade\View;
-use think\middleware\AllowCrossDomain;
+use Closure;
 use think\Request;
 use think\Response;
 
 /**
  * 支持跨域请求中间件
  */
-class Cors extends AllowCrossDomain
+class Cors
 {
     protected $header
         = [
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => 1800,
-            'Access-Control-Allow-Methods'     => '*',
+            'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers'     => '*',
         ];
+
+
+    /**
+     * 允许跨域请求
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     *
+     * @return mixed|Response
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->isOptions()) {
+            return response('', 200, $this->header);
+        }
+
+        return $next($request);
+    }
 }
