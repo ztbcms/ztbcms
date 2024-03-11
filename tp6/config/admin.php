59d18821b6f5f9eba14cd904e04ebb258c14f815
Jayin <tonjayin@gmail.com>
2024-03-11 11:40:15 +0800

    Release 4.5.0

--- tp6/config/admin.php
+++ tp6/config/admin.php
@@ -2,8 +2,8 @@
 
 // 管理后台相关信息
 return [
-    "cms_name"            => "ZTBCMS内容管理系统", //CMS名称
-    "cms_build"           => '202312150105', //CMS流水号
-    "cms_version"         => "4.4.5", //CMS版本号
+    "cms_name"            => "ZTBCMS开发框架", //CMS名称
+    "cms_build"           => '202403111139', //CMS流水号
+    "cms_version"         => "4.5.0", //CMS版本号
     "application_version" => "0.0.0", //产品版本号
 ];
