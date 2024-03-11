6d49ca60d56f95496dd92d03cdcc209a69a3b7d5
Jayin <tonjayin@gmail.com>
2024-03-11 10:42:06 +0800

    config/cache.php: 默认从 env 中获取 redis 配置

--- tp6/config/cache.php
+++ tp6/config/cache.php
@@ -8,36 +8,36 @@ return [
     'default' => env('cache.driver', 'file'),
 
     // 缓存连接方式配置
-    'stores'  => [
-        'file'  => [
+    'stores' => [
+        'file' => [
             // 驱动方式
-            'type'       => 'File',
+            'type' => 'File',
             // 缓存保存目录
-            'path'       => '',
+            'path' => '',
             // 缓存前缀
-            'prefix'     => '',
+            'prefix' => '',
             // 缓存有效期 0表示永久缓存
-            'expire'     => 0,
+            'expire' => 0,
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
-            'serialize'  => [],
+            'serialize' => [],
         ],
         // redis
         'redis' => [
             // 驱动方式
-            'type'       => 'redis',
+            'type' => 'redis',
             // 服务器地址
-            'host'       => '127.0.0.1',
-            'port'       => 6379,
-            'password'   => '',
-            'select'     => 0,
-            'timeout'    => 100,
-            'expire'     => 0,
+            'host' => env('redis.host', '127.0.0.1'),
+            'port' => env('redis.port', '6379'),
+            'password' => env('redis.password', ''),
+            'select' => env('redis.select', 0),
+            'timeout' => env('redis.timeout', 100),
+            'expire' => env('redis.expire', 0),
             'persistent' => false,
-            'prefix'     => '',
-            'tag_prefix' => 'tag:',
-            'serialize'  => [],
+            'prefix' => env('redis.prefix', ''),
+            'tag_prefix' => env('redis.tag_prefix', 'tag:'),
+            'serialize' => [],
         ],
         // 更多的缓存连接
     ],
