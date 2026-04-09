d5470ed3cbfb93f0130be1bc89b1b149876d8487
Jayin Ton <tonjayin@gmail.com>
2026-04-10 02:50:42 +0800

    调整文案

--- tp6/app/demo/Config.inc.php
+++ tp6/app/demo/Config.inc.php
@@ -2,17 +2,17 @@
 
 return [
     //模块名称
-    'modulename'  => '示例',
+    'modulename'  => '示例模块',
     //图标
     'icon'        => 'https://dn-coding-net-production-pp.qbox.me/e57af720-f26c-4f3b-90b9-88241b680b7b.png',
     //模块简介
-    'introduce'   => 'ztbcms 示例',
+    'introduce'   => '包含一些常用示例模块',
     //模块介绍地址
     'address'     => 'http://doc.ztbcms.com/',
     //模块作者
     'author'      => 'Jayin',
     //作者地址
-    'authorsite'  => 'http://ztbcms.com',
+    'authorsite'  => '',
     //作者邮箱
     'authoremail' => 'jayinton@qq.com',
     //版本号，请不要带除数字外的其他字符
